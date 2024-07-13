a0621bc1c9af0e01bd4f5c37c92cc8ee6c26a327
shubhamsadare6626 <shubhamsadaress@gmail.com>
2024-07-13 16:01:12 +0530

    code refactored and spotless applied

---
+++ src/test/java/com/java/spring/batch/statement/BatchProcessApplicationTests.java
@@ -0,0 +1,15 @@
+package com.java.spring.batch.statement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
+}
